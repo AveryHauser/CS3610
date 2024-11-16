22f68e457a9f2b9754e12a0e4b0f2b39eec7343d
AveryHauser <ah236122@ohio.edu>
2024-11-16 15:55:57 -0500

    added project 4

--- project-4/source/main.cpp
+++ project-4/source/main.cpp
@@ -1,60 +1,95 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
 
-int partition(vector<int>& list, int first, int last) {
-  // The pivot should be the median of the
-  // first, middle, and last elements.
+typedef pair<int, pair<int, int> > prs;
 
-  int smallIndex = first;
-  int pivot = list[last];
-  int i = list[first - 1];
+int partition(vector<int>& list, int first, int last) 
+{
+    int mid = first + (last - first) / 2;
 
-  for (int j = 0; j < last -1; j++){
-    if (list[j] < pivot){
-      i++;
-      swap(list[i], list[j]);
+    // Choosing pivot as the median of first, middle, and last elements
+    int pivot = list[mid];
+    swap(list[mid], list[last]); // Move pivot to end
+
+    int smallIndex = first; // Keeps track of the boundary for elements smaller than the pivot
+    for (int i = first; i < last; ++i) 
+    {
+        if (list[i] <= pivot) 
+        {
+            swap(list[i], list[smallIndex]);
+            ++smallIndex;
+        }
+    }
+    swap(list[smallIndex], list[last]); // Move pivot to its correct position
+    return smallIndex;
+}
+
+void quicksort(vector<int>& list, int first, int last) 
+{
+  if (first < last)
+  {
+      int pi = partition(list, first, last);
+      quicksort(list, first, pi - 1);
+      quicksort(list, pi + 1, last);
+  }
+}
+
+void multiway_merge(vector<vector<int> >& input_lists, vector<int>& output_list) 
+{
+  priority_queue<prs, vector<prs>, greater<prs> > p_queue;
+
+  for (size_t i = 0; i < input_lists.size(); i++)
+  {
+    p_queue.push(make_pair(input_lists[i][0], make_pair(i, 0)));
+  }
+  
+  while (!p_queue.empty())
+  {
+    prs curr = p_queue.top();
+    p_queue.pop();
+
+    int i = curr.second.first;
+    int j = curr.second.second;
+    
+    output_list.push_back(curr.first);
+
+    if (j + 1 < input_lists[i].size())
+    {
+      p_queue.push(make_pair(input_lists[i][j + 1], make_pair(i, j + 1)));
     }
   }
-  swap(list[i + 1], list[last]);
-  return (i + 1);
-}
-
-void quicksort(vector<int>& list, int first, int last)
-{
-
-}
-
-void multiway_merge(
-  vector<vector<int> >& input_lists, vector<int>& output_list
-) 
-{
-
 } 
 
-int main(int argc, char** argv) {
-  int n, m;
-  cin >> n >> m;
+int main(int argc, char** argv) 
+{
+    int n, m;
+    cin >> n >> m;
 
-  vector<vector<int> > input_lists(n, vector<int>(m));
+    vector<vector<int> > input_lists(n, vector<int>(m));
 
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j) {
-      cin >> input_lists[i][j];
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            cin >> input_lists[i][j];
+        }
     }
-  }
 
-  // Quicksort k sublists
-  for (int i = 0; i < input_lists.size(); ++i)
-    quicksort(input_lists[i], 0, m-1);
+    // Quicksort k sublists
+    for (int i = 0; i < input_lists.size(); ++i)
+        quicksort(input_lists[i], 0, m - 1);
 
-  // Merge n input sublists into one sorted list
-  vector<int> output_list(n * m);
-  multiway_merge(input_lists, output_list);
+    // Merge n input sublists into one sorted list
+    vector<int> output_list;
+    multiway_merge(input_lists, output_list);
 
-  for (int i = 0; i < output_list.size(); ++i)
-    cout << output_list[i] << " ";
-  cout << endl;
+    for (int i = 0; i < output_list.size(); ++i)
+        cout << output_list[i] << " ";
+    
+    cout << endl;
 
-  return 1;
+    return 0;
 }
